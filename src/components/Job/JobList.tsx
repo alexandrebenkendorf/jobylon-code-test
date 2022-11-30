d67cbf3c3de48ec45db28843eabfc1f312c35587
Alexandre Benkendorf <alexandre.alemao@gmail.com>
2022-11-30 12:32:41 +0000

    chore: add skeleton

--- src/components/Job/JobList.tsx
+++ src/components/Job/JobList.tsx
@@ -1,15 +1,18 @@
 import React, { useMemo, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 import { Input } from '../common/Form';
 import { Section, SectionContainer } from '../common';
+import { useJobs } from '../../hooks/useJobs';
+import { filterJobs } from '../../utils/filterJobs';
 
 import { JobCard } from './JobCard';
 import { FilterItem, FilterWrapper, JobCardListStyled } from './JobList.styles';
-import { useJobs } from './hooks/useJobs';
-import { filterJobs } from './utils';
+import { JobCardWrapper } from './JobCard.styles';
 
 export function JobList(): React.ReactElement {
-  const { jobs } = useJobs();
+  const { jobs, isLoading } = useJobs();
   const [filterTerm, setFilterTerm] = useState('');
   const [filterPlace, setFilterPlace] = useState('');
 
@@ -41,6 +44,17 @@ export function JobList(): React.ReactElement {
           </FilterItem>
         </FilterWrapper>
         <JobCardListStyled>
+          {isLoading && (
+            <Skeleton
+              wrapper={JobCardWrapper}
+              height={100}
+              count={3}
+              borderRadius={10}
+              baseColor="#f9f9f9"
+              highlightColor="#ffffff"
+              style={{ opacity: 0.9 }}
+            />
+          )}
           {jobsFiltered.map((job) => (
             <JobCard key={job.id} job={job} />
           ))}
